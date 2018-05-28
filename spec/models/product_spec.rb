091ea44ad0556905e8b008f968dc355bb8a682b6
imack5 <iainsmackenzie@gmail.com>
2018-05-28 16:31:07 +0000

    finsihed produc specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,8 +4,32 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'saves succesfully' do
       @cat = Category.create(name: "newCat")
-      @product = Product.create(name: "newProd", category: @cat, quantity: 10, price: 20)
+      @product = Product.new(name: "newProd", category: @cat, quantity: 10, price: 20)
       expect(@product).to be_valid
+    end
+
+    it 'is not valid because no name' do
+      @cat = Category.create(name: "newCat")
+      @product = Product.new(name: nil, category: @cat, quantity: 10, price: 20)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid because no category' do
+      @cat = Category.create(name: "newCat")
+      @product = Product.new(name: "newProd", category: nil, quantity: 10, price: 20)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid because no quantity' do
+      @cat = Category.create(name: "newCat")
+      @product = Product.new(name: "newProd", category: @cat, quantity: nil, price: 20)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid because no price' do
+      @cat = Category.create(name: "newCat")
+      @product = Product.new(name: "newProd", category: @cat, quantity: 10, price: nil)
+      expect(@product).to_not be_valid
     end
   end
 end
