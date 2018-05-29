2710ff03f7032d644939bf8bd4e0c6577a9cacd5
imack5 <iainsmackenzie@gmail.com>
2018-05-29 18:08:20 +0000

    Setup specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  describe "Validations" do
-
-    it "is created succesfully" do
-      user = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: "abcdefgh", password_confirmation: "abcdefgh")
+  describe 'Validations' do
+    it 'is created succesfully' do
+      user = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: 'abcdefgh', password_confirmation: 'abcdefgh')
       expect(user).to be_valid
     end
 
-    it "should have a password" do
-      user = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: nil, password_confirmation: "abcdefgh")
+    it 'should have a password' do
+      user = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: nil, password_confirmation: 'abcdefgh')
       expect(user).to_not be_valid
     end
 
-    it  "should have matching passwords" do
-      user = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: "abcdefghi", password_confirmation: "abcdefgh")
+    it 'should have matching passwords' do
+      user = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: 'abcdefghi', password_confirmation: 'abcdefgh')
       expect(user).to_not be_valid
     end
 
-    it "should have a unique email" do
-      user = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: "abcdefghi", password_confirmation: "abcdefgh")
-      user2 = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: "abcdefghi", password_confirmation: "abcdefgh")
+    it 'should have a unique email' do
+      user = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: 'abcdefghi', password_confirmation: 'abcdefgh')
+      user2 = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: 'abcdefghi', password_confirmation: 'abcdefgh')
       expect(user).to_not be_valid
     end
 
-    it "should have minimum password length of 7" do
-      user = User.new(name: "Iain", email: "i_mack5@hotmail.com", password: "oh", password_confirmation: "oh")
+    it 'should have minimum password length of 7' do
+      user = User.new(name: 'Iain', email: 'i_mack5@hotmail.com', password: 'oh', password_confirmation: 'oh')
       expect(user).to_not be_valid
     end
   end
 
   describe '.authenticate_with_credentials' do
-    let!(:user) { User.create! name: "Iain", email: "i_mack5@hotmail.com", password: "abcdefgh", password_confirmation: "abcdefgh" }
+    let!(:user) { User.create! name: 'Iain', email: 'i_mack5@hotmail.com', password: 'abcdefgh', password_confirmation: 'abcdefgh' }
 
     it 'should have a email in the db' do
-      expect(User.authenticate_with_credentials("i@hotmail.com", "abcdefgh")).to equal(nil)
+      expect(User.authenticate_with_credentials('i@hotmail.com', 'abcdefgh')).to equal(nil)
     end
 
     it 'should have a email with a matching password in the db' do
-      expect(User.authenticate_with_credentials("i_mack5@hotmail.com", "abcdefghi")).to equal(nil)
+      expect(User.authenticate_with_credentials('i_mack5@hotmail.com', 'abcdefghi')).to equal(nil)
     end
 
     it 'should return a user when succesful' do
-      expect(User.authenticate_with_credentials("i_mack5@hotmail.com", "abcdefgh")).to_not be_nil
+      expect(User.authenticate_with_credentials('i_mack5@hotmail.com', 'abcdefgh')).to_not be_nil
     end
-
   end
-
 end
